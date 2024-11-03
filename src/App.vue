c96fffa8608f93f021282a5504490e0f3e7e0cfd
Salih Usmanov <salih.usmanovv@gmail.com>
2024-11-03 22:05:25 +0200

    Setup shadcn configuration and integrate with project theme

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input type="text" class="w-full bg-background pl-8" placeholder="Search">
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white">
 
-  <RouterView />
+    </div>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+
 </template>
